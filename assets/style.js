f42b3ec0012bcf7041b2cb9b082c61ecabb62597
octavio <octavioodasso709@gmail.com>
2022-10-25 22:35:09 -0300

    reorganizamos

--- assets/style.js
+++ assets/style.js
@@ -1,176 +1,71 @@
 
-    let nombreMarca = prompt('Ingrese el nombre de la marca')
-    let numero_de_modelos = prompt('Ingrese el numero de modelos que va a agregar');
+if( typeof window === 'object' ){ 
 
+	window.addEventListener('DOMContentLoaded', () => {
 
-    let contador = 0;
-    let listaModelos = [ ]
+		let formulario = document.querySelector('#formPeliculas');
 
+		formulario.addEventListener('submit', ( event ) => {
 
+			event.preventDefault();
 
-class Concesionaria {
-    
+			let tituloPelicula = document.querySelector('#addPelicula').value;
+	
+		
+			if ( tituloPelicula.length >= 1 ) {
+				
+				localStorage.setItem( tituloPelicula, tituloPelicula);
+				location.reload();
+			}else{
+				alert('Debe ingresa una película')
+			}
+	
+		});
+	
+	
+		let ul = document.querySelector('#listaPeliculas');
 
-    constructor( nombre, modelo) {
+		ul.classList.add('list-group', 'list-group-flush');
 
-        this._nombre = function () {
-            return nombre;
-        };
+	
+		for( let i in localStorage ){
+			
+			if( typeof localStorage[i] == 'string'){
+				let li = document.createElement('li');
+				li.classList.add('list-group-item');
+				li.append( localStorage[i] );
+				ul.append(li);
+			}
+	
+		}
 
-        this._modelo = function () {
-            return modelo || [];
-        };
+		
+	
+	
+		let removeFormulario = document.querySelector('#formBorrarPeliculas');
+	
+		removeFormulario.addEventListener('submit', ( event ) => {
+			
 
-    }
-    
-    getName() {
-        return this._nombre();
-    }
+			let tituloPelicula = document.querySelector('#removePelicula').value;
+	
+		
+			if ( tituloPelicula.length >= 1 ) {
+				
+				localStorage.removeItem(tituloPelicula);
+				location.reload();
+			}
+	
+	
+		});
 
-    setName( nuevo_nombre ) {
-        this._nombre = function () {
-            return nuevo_nombre;
-        };
-    }
+		
+		let usuario = {
+			nombre: 'octavio',
+			email: 'octavioodasso709@gmail.com',
+		}
 
-    agregarPaciente( modelo ) {
-        this.paciente.push(modelo);
-    }
+		localStorage.setItem('usuario',  usuario );
 
-    getModelo() {
-
-        this._paciente().forEach(function ( elemento, index ) {
-            console.log('--------------------------------------------')
-            console.log(`Numero de Modelo: ${index}`);
-            console.log(`Nombre: ${elemento._nombre()}`);
-            console.log(`Motor: ${elemento._motor()}`);
-            console.log(`Combustible: ${elemento._combustible()}`);
-            console.log(`Valor modelo: ${elemento._valorModelo()}`);
-        });
-
-    }
-
-    getValorTotalModelo(){
-
-        let valorModelo = [ ]
-
-        this._modelo().forEach(function ( elemento, index) {
-            valorAtenciones.push(elemento._valorModelo());
-        });
-        console.log('--------------------------------------------')
-        console.log('Valor total de modelo', valorModelo.reduce( ( valorAnterior, valorActual ) => valorAnterior+valorActual, 0 ))
-    }
-
-    getModeloNombre( busquedaDeNombre ) {
-
-        this._modelo().forEach(function (elemento, index) {
-
-            if (elemento._nombre() == busquedaDeNombre) {
-
-                console.log('--------------------------------------------')
-                console.log(`Numero de modelo: ${index}`);
-                console.log(`Nombre: ${elemento._nombre()}`);
-                console.log(`Motor: ${elemento._motor()}`);
-                console.log(`Combustible: ${elemento._combustible()}`);
-                console.log(`Valor modelo: ${elemento._valorModelo()}`);
-
-            }
-
-        });
-
-    }
+	})
 }
- 
-class Modelo {
-
-    
-    constructor( nombre, motor, combustible, valorModelo) {
-
-        this._nombre = function () {
-            return nombre;
-        };
-
-        this._motor = function () {
-            return motor;
-        };
-
-        this._combustible = function () {
-            return combustible;
-        };
-
-        this._valorModelo = function () {
-            return valorModelo;
-        };
-
-    }
-
-    getName() {
-        return this._nombre();
-    }
-    getMotor() {
-        return this._motor();
-    }
-    getCombustible() {
-        return this._combustible();
-    }
-    getValormodelo() {
-        return this._valorModelo();
-    }
-
-    setName(nuevo_nombre) {
-        this._nombre = function () {
-            return nuevo_nombre;
-        };
-    }
-    setMotor(nueva_motor) {
-        this._motor = function () {
-            return nueva_motor;
-        };
-    }
-    setCombustible(nuevo_combustible) {
-        this._combustible = function () {
-            return nuevo_combustible;
-        };
-    }
-}
-
-
-
-while( contador < numero_de_modelos ){
-
-      
-      let nombre = prompt('Ingrese el nombre del modelo')
-      let motor = parseInt(prompt('Ingrese el tamaño del modelo'))
-      let combustible = prompt('Ingrese el tipo de combustible que utiliza')
-      let valorModelo = parseInt(prompt('Ingrese Valor del modelo'))
-
-      let modelo = new Modelo( nombre, motor, combustible, valorModelo )
-      
-      listaModelos.push( modelo )
-      
-   
-      contador++
-
-}
-
-let buscar = prompt('Ingrese el nombre del Modelo que quiere buscar')
-
-const imprimirMarcas = ( marca, modelo, buscar )=>{
-    
-    console.log( 'Nombre de la marca:', marca.getName() )
-    console.log('=================NOMBRE DE Modelos====================')
-    
-    for(let marca of marca){
-        console.log('Salida de modelos', modelo._nombre())
-    }
-    console.log('=================SALIDA DE MODELO POR MARCA====================')
-    consultorio.getModelo() 
-    console.log('=================SALIDA DE MODELO BUSCADO POR NOMBRE====================')
-    consultorio.getName(buscar);
-    console.log('=================PRECIO TOTAL Modelo====================')
-    consultorio.getValorTotalModelo();
-}
-
-let marca = new Marca( nombreMarca, listaModelo )
-
-imprimirMarca( marca,listaModelos, buscar )
